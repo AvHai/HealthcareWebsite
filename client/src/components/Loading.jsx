f090d8b88e951eb47b9331a76303971fef352645
Avhishek Nandi <avhihseknandi03@gmail.com>
2025-07-03 16:00:46 +0530

    admin functionality

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+const Loading= () => {
+  return (
+   <DotLottieReact
+      src="https://lottie.host/c35add2f-a03f-4d62-8739-9da27efc6a82/nTlIj1fO8U.lottie"
+      loop
+      autoplay
+    />
+  );
+};
+export default Loading;
